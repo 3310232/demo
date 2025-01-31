bc8e273402d634d3840de7dcc9de0de307a688ce
keep doing <wangqihzx@qq.com>
2025-01-31 22:36:02 +0800

    update database

--- express/express.js
+++ express/express.js
@@ -1,5 +1,21 @@
 const express = require('express')
 const app = express()
+const mysql = require('mysql2')
+const db = mysql.createPool({
+    host:'127.0.0.1',
+    // post:'80',
+    user:'root',
+    password:'123456',
+    database:'my_db_01'
+})
+
+db.query('select * from users',(err,res)=>{
+    if(err){
+        return console.log(err.message)
+    }else{
+        return console.log(res)
+    }
+})
 const cors = require('cors')
 app.use(cors())
 const example = require('./example')
